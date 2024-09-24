38f7d1d2303f46fce54efc7555737de18d4834b9
Deepak <ds262969@gmail.com>
2024-09-25 04:34:20 +0530

    U.I of the login page completed successfully

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,32 +1,48 @@
 import "./Login.css";
 import assets from "../../assets/assets";
+import { useState } from "react";
 const Login = () => {
+  const [currState, setCurrState] = useState("Sign up");
   return (
     <div className="login">
       <img src={assets.logo_big} alt="" className="logo" />
       <form className="form-login">
-        <h2>Sign Up</h2>
-        <input
-          type="text"
-          placeholder="Enter Username"
-          className="form-input"
-          required
-        />
+        <h2>{currState}</h2>
+        {currState === "Sign up" ? (
+          <input
+            type="text"
+            placeholder="Enter Username"
+            className="form-input"
+            required
+          />
+        ) : null}
         <input type="email" placeholder="Enter Email" className="form-input" />
         <input
           type="password"
           placeholder="Enter Password"
           className="form-input"
         />
-        <button type="submit">Sign Up</button>
+        <button type="submit">
+          {currState === "Sign up" ? "Create Account" : "Login now"}
+        </button>
         <div className="login-term">
           <input type="checkbox" placeholder="" />
           <p>Agree to the terms of use & privacy policy</p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">
-            Already have an account <span>Click Here</span>
-          </p>
+          {currState === "Sign up" ? (
+            <p className="login-toggle">
+              Already have an account{" "}
+              <span onClick={() => setCurrState("Login")}>
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className="login-toggle">
+              Already have an account{" "}
+              <span onClick={() => setCurrState("Sign up")}>Create an account</span>
+            </p>
+          )}
         </div>
       </form>
     </div>
