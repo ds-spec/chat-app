dc8d69aedd7c5e1085c46f569c03981df6f4a121
Deepak <ds262969@gmail.com>
2024-09-25 05:43:34 +0530

    LeftSidebar completed successfully

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,7 +1,18 @@
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import Chatbox from "../../components/Chatbox/Chatbox";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
 import "./Chat.css";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat-container">
+        <LeftSidebar />
+        <Chatbox />
+        <RightSidebar />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
