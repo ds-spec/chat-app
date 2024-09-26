e562ac85e7ed40195f6edcdedbca3b836fec5a82
Deepak <ds262969@gmail.com>
2024-09-27 04:45:49 +0530

    U.I for the Chatbox completed

--- src/components/Chatbox/Chatbox.jsx
+++ src/components/Chatbox/Chatbox.jsx
@@ -1,5 +1,55 @@
+import assets from "../../assets/assets";
+import "./Chatbox.css";
+
 const Chatbox = () => {
-  return <div>Chatbox</div>;
+  return (
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>
+          Richard Sanford <img className="dot" src={assets.green_dot} alt="" />
+        </p>
+        <img src={assets.help_icon} className="help" alt="" />
+      </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">
+            lorem ispum placeholder text is commonly used ....
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img className="msg-img" src={assets.pic1} alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">
+            lorem ispum placeholder text is commonly used ....
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="chat-input">
+        <input type="text" placeholder="Type a message..." />
+        <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Chatbox;
